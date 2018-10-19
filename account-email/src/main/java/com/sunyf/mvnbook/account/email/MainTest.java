647e5ef177889a1031d605bd24c5a1dc5cefdd21
sunyifeng <9339996@qq.com>
2018-10-19 16:58:15 +0800

    add packaging pom

---
+++ account-email/src/main/java/com/sunyf/mvnbook/account/email/MainTest.java
@@ -0,0 +1,13 @@
+package com.sunyf.mvnbook.account.email;
+
+/**
+ * @program: maven-in-action
+ * @description:
+ * @author: Mr.Sun
+ * @create: 2018-10-19 16:42
+ **/
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.printf("test sunyf!");
+    }
+}
