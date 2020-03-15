4e4a34feeeace896d8d8244953c2563891c077d5
sunyi-feng <9339996@qq.com>
2020-03-16 00:58:56 +0800

    maven仓库中的LastUpdated文件生成原因及删除

--- cleanLastUpdated.bat
+++ cleanLastUpdated.bat
@@ -2,7 +2,7 @@
 rem create by sunyf https://blog.csdn.net/u011990675/article/details/80066897
   
 rem 这里写你的仓库路径
-set REPOSITORY_PATH=D:\repository
+set REPOSITORY_PATH=C:\Users\sunyifeng\.m2\repository
 rem 正在搜索...
 for /f "delims=" %%i in ('dir /b /s "%REPOSITORY_PATH%\*lastUpdated*"') do (
     echo %%i
